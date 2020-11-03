110fd05dc950e6ff7fb967ab534ad9733d017287
Carolista <cjones.renaissancewoman@gmail.com>
2020-11-03 13:53:05 -0600

    Added content to index, styles, and app component files

---
+++ angular-basics/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  
+  // All variables and methods MUST go inside the class!
+
+  pageTitle = 'Sorbet vs Sherbet';
+  image = '/assets/images/sorbet.png'
+
+  
+
+}
