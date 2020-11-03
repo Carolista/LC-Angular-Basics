91e692e4c30666a8cb4a116c18d2ff42e14c6f51
Carolista <cjones.renaissancewoman@gmail.com>
2020-11-03 16:14:58 -0600

    finished content and added instructions

--- angular-basics/src/app/app.component.ts
+++ angular-basics/src/app/app.component.ts
@@ -8,11 +8,25 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   
-  // All variables and methods MUST go inside the class!
+  // All fields (properties) and methods must go INSIDE the class!
 
-  pageTitle = 'Sorbet vs Sherbet';
-  image = '/assets/images/sorbet.png'
+  /*  
+    Remember that Angular uses TypeScript, so variables and methods must be "typed" as a string, number, boolean, etc. Also notice how the fields (TypeScript variables) are NOT declared with 'let', and methods are NOT declared with 'function'. BUT once you are inside a method's brackets {}, you are using regular old JavaScript! Just make sure to refer to the class's global fields and methods with 'this.property' or 'this.method()'.
+  */
 
-  
+  // Declare fields (as strings) called 'thingOne' and 'thingTwo' for the two things you'll be writing about.
+  thingOne: string = 'Sorbet';
+  thingTwo: string = 'Sherbet';
 
-}
+  // Declare a field called 'imageURL' for the path to your image (a string). Under 'src' in the file tree, you'll see a folder called 'assets', and another folder inside that called 'images.' That's where you should put your image file.
+  imageURL: string = '/assets/images/sorbet.png'
+
+  // Here's a quick function to take thingOne & thingTwo and create your title.
+  createTitle(): string {
+    let pageTitle = `${this.thingOne} vs ${this.thingTwo}`; // standard template literal
+    return pageTitle;
+  }
+
+  // Could we have just had a single field hold the whole title name? Yes. But then we wouldn't have a handy little example of how to write a method and display it in the HTML template! :-)
+
+} // End of class
